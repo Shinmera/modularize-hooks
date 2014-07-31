876f335a74a80b0ecb8627e0c83f4a3c2d1d7ad4
Shinmera <shinmera@tymoon.eu>
2014-07-31 09:30:49 +0200

    Completed documentation

--- hooks.lisp
+++ hooks.lisp
@@ -7,23 +7,33 @@
 (in-package #:org.tymoonnext.radiance.lib.modularize.hooks)
 
 (defun make-hook-package-name (module)
+  "Returns a fitting hooks package name for the given module."
   (format NIL "~a.HOOKS" (module-identifier module)))
 
 (defun hook-package (module)
+  "Returns the hooks package of the module or signals an error if none can be found."
   (or (find-package (make-hook-package-name module))
       (error "Module is not set up for hooks.")))
 
 (defun transform-symbol (module-symbol)
+  "Transforms the symbol into one from the module's hooks package."
   (let ((package (hook-package (symbol-package module-symbol))))
     (or (find-symbol (symbol-name module-symbol) package)
         (intern (symbol-name module-symbol) package))))
 
 (defun hookify (&optional (module *package*))
+  "Turns the module into one capable of hooks.
+
+In essence this merely defines a new package with a matching name."
   (let ((name (make-hook-package-name module)))
     (unless (find-package name)
       (make-package name :use ()))))
 
 (defun dehookify (&optional (module *package*))
+  "Returns the module to one that is not capable of hooks.
+
+In essence this merely removes all functions from the hooks package
+and deletes it."
   (let ((package (hook-package module)))
     (do-symbols (symbol package)
       (when (fboundp symbol)
@@ -37,6 +47,7 @@
   (dehookify module))
 
 (defun call-all-methods (gf &rest args)
+  "Calls all methods of the generic function with the given arguments."
   (loop for method in (c2mop:generic-function-methods gf)
         unless (method-qualifiers method)
           do (let ((ident (first (c2mop:method-specializers method))))
@@ -44,6 +55,8 @@
                  (apply gf (c2mop:eql-specializer-object ident) args)))))
 
 (defmacro define-hook (name args &optional documentation)
+  "Defines a new hook on which triggers can be defined.
+The name should be a symbol from the module that the hook should belong to."
   (assert (symbolp name))
   (let ((name (transform-symbol name)))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
@@ -53,12 +66,14 @@
          (call-all-methods #',name ,@(extract-lambda-vars args))))))
 
 (defun remove-hook (name)
+  "Removes the hook as named."
   (fmakunbound (transform-symbol name)))
 
 (defun %trigger (hook &rest args)
   (apply (symbol-function (transform-symbol hook)) NIL args))
 
 (defun trigger (hook &rest args)
+  "Calls all triggers registered on the hook with the given arguments."
   (apply #'%trigger hook args))
 
 (define-compiler-macro trigger (hook &rest args)
@@ -67,6 +82,11 @@
       `(%trigger ,hook ,@args)))
 
 (defmacro define-trigger (hook args &body body)
+  "Defines a new trigger on the hook.
+A trigger can either accept no arguments or it has to match the hook in its arguments list.
+The name of the trigger defaults to the *PACKAGE*. If you want to have multiple triggers for
+the same hook in the same package, use a list of the following structure as the HOOK argument:
+ (hook trigger-name)"
   (destructuring-bind (name ident) (if (listp hook) hook (list hook *package*))
     (let* ((name (transform-symbol name))
            (realargs (cdr (function-arguments (symbol-function name)))))
@@ -79,6 +99,7 @@
   `(defmethod ,(transform-symbol hook) ,@args))
 
 (defun remove-trigger (hook &optional ident specializers)
+  "Attempts to remove the trigger from the hook."
   (let* ((name (transform-symbol hook))
          (func (symbol-function name))
          (ident (or ident *package*))
--- toolkit.lisp
+++ toolkit.lisp
@@ -7,18 +7,22 @@
 (in-package #:org.tymoonnext.radiance.lib.modularize.hooks)
 
 (defun lambda-keyword-p (symbol)
+  "Returns the symbol if it is a lambda-keyword symbol (the &-options)."
   (find symbol '(&allow-other-keys &aux &body &environment &key &optional &rest &whole)))
 
 (defun remove-aux-part (lambda-list)
+  "Removes the &aux part of the lambda-list."
   (let ((position (position '&aux lambda-list)))
     (if position
         (subseq lambda-list 0 position)
         lambda-list)))
 
 (defun flatten-lambda-list (lambda-list)
+  "Flattens the lambda-list by replacing all lists within it with their respective first symbol."
   (mapcar #'(lambda (a) (if (listp a) (car a) a)) lambda-list))
 
 (defun extract-lambda-vars (lambda-list)
+  "Extracts the symbols that name the variables in the lambda-list."
   (remove-if #'lambda-keyword-p (flatten-lambda-list (remove-aux-part lambda-list))))
 
 (defun function-arguments (function)
@@ -29,6 +33,7 @@ This is only implemented with: SBCL, SWANK, working FUNCTION-LAMBDA-EXPRESSION."
   #-(and sbcl swank) (second (nth-value 2 (function-lambda-expression function))))
 
 (defun required-lambda-vars (lambda-list)
+  "Returns the list of required variables in the lambda-list."
   (loop for i in (if (eql '&whole (first lambda-list))
                      (cddr lambda-list)
                      lambda-list)
@@ -36,6 +41,8 @@ This is only implemented with: SBCL, SWANK, working FUNCTION-LAMBDA-EXPRESSION."
         collect i))
 
 (defun function-lambda-matches (function lambda-list)
+  "Returns T if the function matches the lambda-list in arguments.
+As a secondary value it returns a reason as to why it may have failed the test."
   (let ((lambda-cur (remove-aux-part (function-arguments function)))
         (lambda-act (remove-aux-part lambda-list)))
     (cond
